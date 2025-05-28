f24f1592985fa67e1108d7781da36b6e941577f7
Carlos Awo <awocarlos55@gmail.com>
2025-05-28 10:48:14 +0100

    add home page and tailwind cdn

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   put "/articles/:id", to: "articles#update"
 
   delete "/articles/:id", to: "articles#destroy"
+
+  root "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
